ba2bb8ae493e91c5b233587314616545fc5295d8
Michael Wall <mjwall@gmail.com>
2012-03-28 22:08:08 -0400

    Ok, didn't go to bed
    All script running now, makefile updated.  Really going to bed this time.

--- test/all.sh
+++ test/all.sh
@@ -2,4 +2,14 @@
 
 # just a simple file to aggregate all tests
 
-. ./test-install.sh
+SCRIPT_DIR=$(dirname $0)
+
+run_tests() {
+    local file=$1
+    echo -n -e "\033[0;32mRunning ${file}\033[0m\n"
+    ./"${SCRIPT_DIR}"/"${file}"
+}
+
+run_tests "test-install.sh"
+
+
